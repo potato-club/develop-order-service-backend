de8dcc35369f28de409b40133c20a066746ec110
JOE1534 <king01286@naver.com>
2023-01-15 12:38:29 +0900

    Initial commit

---
+++ src/main/java/com/server/dos/DosApplication.java
@@ -0,0 +1,13 @@
+package com.server.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/dos/DosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.dos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

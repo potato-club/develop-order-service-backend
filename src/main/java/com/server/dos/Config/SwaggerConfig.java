9ae7e2f51df53772ab48af355e8e035b41819b45
JOE1534 <king01286@naver.com>
2023-01-22 21:17:22 +0900

    #2 [feat] Swagger를 위한 springfox 라이브러리 추가 및 Config 파일 생성

---
+++ src/main/java/com/server/dos/Config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.server.dos.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    private String version = "v0.1";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DOS API")
+                .description("DOS 프로젝트 API 사용법 정리")
+                .version(version)
+                .build();
+    }
+}

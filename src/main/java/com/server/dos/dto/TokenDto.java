5d2ba74ede22aeb02ece803a9b46d82d589e7f58
haeyeon0106 <yeon99016@gmail.com>
2023-02-19 21:12:43 +0900

    Jwt 생성 #1

--- src/main/java/com/server/dos/dto/TokenDto.java
+++ src/main/java/com/server/dos/dto/TokenDto.java
@@ -1,12 +1,14 @@
 package com.server.dos.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class TokenDto {
 
     private String accessToken;

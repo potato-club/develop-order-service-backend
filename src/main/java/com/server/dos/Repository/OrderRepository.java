b87dc6e32833d64e48b6c0db8471061fe7891a87
haeyeon0106 <yeon99016@gmail.com>
2023-02-24 01:11:31 +0900

    pull update

--- src/main/java/com/server/dos/Repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.server.dos.Repository;
-
-import com.server.dos.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}

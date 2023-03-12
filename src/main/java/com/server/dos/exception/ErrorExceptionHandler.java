e64527cb4974b3e446ed6e83a34e5d01ffaa8e42
CJ-Park <king01286@naver.com>
2023-03-13 01:41:53 +0900

    #10 [feat] Order 미팅 시간 중복 시 충돌 예외 처리

--- src/main/java/com/server/dos/exception/ErrorExceptionHandler.java
+++ src/main/java/com/server/dos/exception/ErrorExceptionHandler.java
@@ -3,6 +3,7 @@ package com.server.dos.exception;
 import com.server.dos.exception.custom.OrderException;
 import com.server.dos.exception.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +14,13 @@ public class ErrorExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleOrderException(OrderException ex) {
         log.error("Order Exception", ex);
+        ErrorResponse response = new ErrorResponse(ex.getErrorCode().getCode(), ex.getErrorMessage());
+        return new ResponseEntity<>(response, ex.getErrorCode().getStatus());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleMeetingException(OrderException ex) {
+        log.error("Meeting Exception", ex);
         ErrorResponse response = new ErrorResponse(ex.getErrorCode().getCode(), ex.getErrorMessage());
         return new ResponseEntity<>(response, ex.getErrorCode().getStatus());
     }

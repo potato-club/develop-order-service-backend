8f78704ebc5eefc81ffbca299eb251f987594dfc
haeyeon0106 <yeon99016@gmail.com>
2023-03-20 00:19:52 +0900

    Kakao login thumbnail 해결 #4

--- src/main/java/com/server/dos/entity/user/OAuth2Attribute.java
+++ src/main/java/com/server/dos/entity/user/OAuth2Attribute.java
@@ -45,7 +45,7 @@ public class OAuth2Attribute {
         return OAuth2Attribute.builder()
                 .name((String)kakaoProfile.get("nickname"))
                 .email((String) kakaoAccount.get("email"))
-                .picture((String)kakaoProfile.get("thumbnail_image"))
+                .picture((String)kakaoProfile.get("profile_image_url"))
                 .attributes(kakaoAccount)
                 .attributeKey(attributeKey)
                 .build();

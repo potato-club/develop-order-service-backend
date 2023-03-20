fdd430fa8e6c864dd6643a78ae704083e1bb7919
CJ-Park <king01286@naver.com>
2023-03-20 22:20:57 +0900

    #1 [feat] 완료된 OrderDetail id로 찾아서 가져오는 쿼리 메소드 생성

--- src/main/java/com/server/dos/repository/OrderDetailRepository.java
+++ src/main/java/com/server/dos/repository/OrderDetailRepository.java
@@ -5,10 +5,13 @@ import com.server.dos.entity.OrderDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
     Page<OrderDetail> findOrderDetailsByStateOrderByIdDesc(OrderState state, Pageable pageable);
     List<OrderDetail> findTop5ByOrderByLikesDesc();
+    @Query("select d from OrderDetail d where d.id=:id and d.state=com.server.dos.Enum.OrderState.COMPLETE")
+    OrderDetail findCompleteById(Long id);
 }

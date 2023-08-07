71d8541e4ca8183802a4771993dafc7c5ba3ee76
CJ-Park <king01286@naver.com>
2023-08-07 20:03:02 +0900

    #1 [feat] 회원 탈퇴 시 Order의 User 데이터 지우기 위한 로직 추가

--- src/main/java/com/server/dos/repository/OrderRepository.java
+++ src/main/java/com/server/dos/repository/OrderRepository.java
@@ -2,13 +2,17 @@ package com.server.dos.repository;
 
 import com.server.dos.Enum.OrderState;
 import com.server.dos.entity.Orders;
+import com.server.dos.entity.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     Orders findBySiteName(String siteName);
+    List<Orders> findByUser(User user);
 
     @Query("SELECT o FROM Orders o JOIN o.detail od WHERE od.state = :state")
     Page<Orders> findByOrderDetailState(Pageable pageable, OrderState state);
